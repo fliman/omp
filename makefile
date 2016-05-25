3f44955ce09814003f736b94e24993307962343c
Yu Wang <ywang@sprgpu951.int.cggveritas.com>
2016-05-25 18:22:00 +0800

    add test code

---
+++ makefile
@@ -0,0 +1,8 @@
+
+
+
+omptest:omptest.cpp 
+	g++ omptest.cpp -o $@
+
+clean:
+	rm omptest
---
+++ omptest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <cstring>
+#include <cmath>
+
+
+
+int main(){
+   std::cout<<"OMP test\n";
+   size_t nrow = 256;
+   size_t ncol = 256 * 2;
+   size_t dim = nrow * ncol;	  
+   double *D = new double[dim];
+   memset(D, 0, sizeof(double) * dim);	
+   for(int i = 0; i < nrow; i++){
+	D[i*ncol] = 1 / sqrt(nrow);
+        for(int j = 1; j < ncol / 2; j++ ){
+            double v = cos(i * M_PI * j /nrow);
+            D[i*ncol + j] = v;
+        }
+   }
+    	
+
+   
+
+   delete [] D;
+
+}
